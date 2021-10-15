164473b0501b75b0d1ac1e982139fec38214cb1d
Arthur Dooner <shunderpooch@gmail.com>
2021-10-14 20:38:17 -0400

    Demo with markdown formatting working

---
+++ pages/blog.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import fs from "fs";
+import path from "path";
+
+const Home: NextPage<{
+  source: MDXRemoteSerializeResult<Record<string, unknown>>;
+}> = ({ source }) => {
+  return (
+    <div>
+      <MDXRemote {...source} />
+    </div>
+  );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  // MDX text - can be from a local file, database, anywhere
+
+  const filePath = path.join(process.cwd(), "posts/", "theo-rocks.md");
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  const mdxSource = await serialize(fileContents);
+  return { props: { source: mdxSource } };
+}
